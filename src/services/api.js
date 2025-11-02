8d283ad8843fca0c304d66b0a444584dcf4257b0
nagymedo25 <nagymedo25@gmail.com>
2025-11-02 20:23:33 +0200

    Update API base URL to include '/api/'

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = "https://evolve-nova-back.vercel.app/";
+const API_BASE_URL = "https://evolve-nova-back.vercel.app/api/";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
